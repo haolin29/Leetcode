8e3b942d54938337bcbe45154ffa2e1afe495079
haolin29 <haolin29@gmail.com>
2016-02-17 16:39:55 -0500

    First commit

---
+++ backtracking/294. Flip Game II.java
@@ -0,0 +1,19 @@
+// find all results
+// see if the opponent can win or not, if the opponent cannot win, great, we win!
+
+
+public class Solution {
+    public boolean canWin(String s) {
+        if (s == null || s.length() < 2) return false;
+
+        for (int i = 0; i < s.length() - 1; i++) {
+        	if (s.startsWith("++",i)) {
+        		String t = s.substring(0,i) + "--" + s.substring(i + 2);
+        		if (!canWin(t)) return true;
+
+        	}
+        }
+
+        return false;
+    }
+}
