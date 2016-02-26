10e02798073999ff4da3c24aba1eaa551a7429fc
haolin29 <haolin29@gmail.com>
2016-02-25 22:16:31 -0500

    first commit

---
+++ dynamic programming/300. Longest Increasing Subsequence.java
@@ -0,0 +1,30 @@
+public class Solution {
+    public int lengthOfLIS(int[] nums) {
+        if (nums.length <= 1) {
+            return nums.length;
+        }
+        int n = nums.length;
+        int[] dp = new int[n];
+        
+        for (int i = 0; i < n; i++) {
+            dp[i] = 1;
+        }
+        
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                if (nums[j] < nums[i]) {
+                    if (dp[j] + 1 > dp[i]) {
+                        dp[i] = dp[j] + 1;
+                    }
+                }
+            }
+        }
+        
+        int result = dp[0];
+        for (int i = 1; i < n; i++) {
+            result = Math.max(result, dp[i]);
+        }
+        
+        return result;
+    }
+}
