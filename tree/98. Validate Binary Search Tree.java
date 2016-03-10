35b7c7d9b9e015c9df3c521b3bae8de1c4d94606
haolin29 <haolin29@gmail.com>
2016-03-10 17:08:18 -0500

    Add divide and conquer approach

--- tree/98. Validate Binary Search Tree.java
+++ tree/98. Validate Binary Search Tree.java
@@ -25,3 +25,21 @@ public class Solution {
 
 	}
 }
+
+// divide and conquer 
+
+public class Solution {
+	public boolean isValidBST(TreeNode root) {
+		return isValidBST(root, Integer.MIN_VALUE, Integer.MAX_VALUE);
+	}
+
+	public boolean isValidBST(TreeNode root, min, max) {
+		if (root == null) return true;
+
+		if (root.val <= min || root.val >= max) {
+			return false;
+		}
+
+		return isValidBST(root.left, min, root.val) && isValidBST(root.right, root.val, max);
+	}
+}
