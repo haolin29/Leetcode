98ea2bb72739580eb6cac81b93ef43d24d2346f1
Haolin Ju <hj836@nyu.edu>
2016-07-18 21:22:11 -0400

    [32. Longest Valid Parentheses][Time Limit Exceeded]committed by LeetCode Extension

--- 32-Longest-Valid-Parentheses/solution.java
+++ 32-Longest-Valid-Parentheses/solution.java
@@ -1,36 +1,31 @@
 public class Solution {
     public int longestValidParentheses(String s) {
-  
-    if (s == null) {
-            return 0;
-        }
-
-        Stack<Integer> stack = new Stack<Integer>();
-        int maxLen = 0;
-        int count = 0;
-
+        if(s == null) return 0;
         
-        for (int i = 0; i < s.length(); i++ ) {
-            if (s.charAt(i) == '(' ) {
+        Stack<Integer> stack = new Stack<>();
+        
+        int start = 0;
+        int max = 0;
+        for(int i = 0; i < s.length(); i++) {
+            if(s.charAt(i) == '(') {
                 stack.push(i);
             } else {
-                if (stack.isEmpty()) {
-                    count = 0;
+                if(stack.isEmpty()) {
+                    start = 0;
                 } else {
-                    int matchLen = i - stack.pop() + 1;
-                    
-                    if (stack.isEmpty()) {
-                        count += matchLen;
-                        matchLen = count;
+                    int matchLens = i - stack.pop() + 1;
+                    if(stack.isEmpty()) {
+                        matchLens += start;
+                        start = matchLens;
                     } else {
-                        matchLen = i - stack.peek();
+                        matchLens = i - stack.peek();
                     }
                     
-                    maxLen = Math.max(maxLen, matchLen);
+                    max = Math.max(max, matchLens);
                 }
+                
             }
         }
-        
-        return maxLen;
+        return max;
     }
 }
