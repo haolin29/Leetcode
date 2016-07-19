7dd709d8ada080ef7498fd0971cb79beba3b2cd9
Haolin Ju <hj836@nyu.edu>
2016-07-19 15:41:41 -0400

    [115. Distinct Subsequences][Runtime Error]committed by LeetCode Extension

---
+++ 115-Distinct-Subsequences/solution.java
@@ -0,0 +1,30 @@
+public class Solution {
+    public int numDistinct(String s, String t) {
+        if(s == null || t == null) {
+            return -1;
+        }
+        
+        int m = s.length();
+        int n = t.length();
+        
+        int[][] dp = new int[m+1][n+1];  // s的前i项选出t的前j项有多少种方案
+        
+        // s里面选空串的方案
+        for(int i = 0; i <= m; i++) {
+            dp[i][0] = 1;
+        }
+        
+        for(int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if(s.charAt(i) == t.charAt(j)) {
+                    dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j];
+                } else {
+                    dp[i][j] = dp[i - 1][j];
+                }
+            }
+        }
+        
+        return dp[m][n];
+        
+    }
+}
