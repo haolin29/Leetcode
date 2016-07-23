cf170651c75452d20570d3426d374b0162095640
Haolin Ju <hj836@nyu.edu>
2016-07-22 21:04:43 -0400

    [213. House Robber II][Runtime Error]committed by LeetCode Extension

--- 213-House-Robber-II/solution.java
+++ 213-House-Robber-II/solution.java
@@ -1,5 +1,6 @@
 public class Solution {
-    public int rob(int nums[]) {
+    public int rob(int[] nums) {
+        if(nums == null || nums.length == 0) return 0;
         if (nums.length == 1) return nums[0];
 
         return Math.max(helper(nums, 0, nums.length - 2), helper(nums, 1, nums.length - 1));
