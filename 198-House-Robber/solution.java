5fa8a7838cad1dcfecbf4f8bd11943620d43ca26
Haolin Ju <hj836@nyu.edu>
2016-07-22 20:29:35 -0400

    [198. House Robber][Accepted]committed by LeetCode Extension

--- 198-House-Robber/solution.java
+++ 198-House-Robber/solution.java
@@ -1,22 +1,26 @@
 public class Solution {
     public int rob(int[] nums) {
-        if(nums == null || nums.length == 0) {
+        if (nums == null || nums.length == 0 ) {
             return 0;
         }
-        
+
         int n = nums.length;
-        
-        int[] rob = new int[n]; // define: before i I can rob max money
-        rob[0] = nums[0];
-        
-        for(int i = 1; i < n; i++) {
-            if(i == 1) {
-                rob[1] = Math.max(nums[0], nums[1]);
-            } else {
-                rob[i] = Math.max(rob[i - 1], rob[i - 2] + nums[i]);
-            }
+
+        if (nums.length == 1) {
+            return nums[0];
         }
+
         
-        return rob[n-1];
+
+        int[] dp = new int[n];
+
+        dp[0] = nums[0];
+        dp[1] = Math.max(nums[0], nums[1]);
+
+        for (int i = 2; i < n; i++) {
+            dp[i] = Math.max(dp[i - 1], dp[i - 2] + nums[i]);
+        }
+
+        return dp[n - 1];
     }
 }
