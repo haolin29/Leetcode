c5ce7a94484edc21f63a43c476986d86467973ed
Haolin Ju <hj836@nyu.edu>
2016-07-31 20:18:29 -0400

    [235. Lowest Common Ancestor of a Binary Search Tree][Compile Error]committed by LeetCode Extension

--- 235-Lowest-Common-Ancestor-of-a-Binary-Search-Tree/solution.java
+++ 235-Lowest-Common-Ancestor-of-a-Binary-Search-Tree/solution.java
@@ -12,14 +12,12 @@ public class Solution {
         if(root == null || p == null || q == null) {
             return null;
         }
-        TreeNode large = (p > q ? p : q);
-        TreeNode small = (p < q ? p : q);
-        if(root.val > large) {
-            return lowestCommonAncestor(root.left, p,q);
-        } else if (root.val < small) {
-            return lowestCommonAncestor(root.right, p,q);
-        } else if(root.val < large.val && root.val > small.val) {
+        if (root.val > p.val && root.val < q.val) {
             return root;
+        } else if (root.val > p.val && root.val > q.val) {
+            return lowestCommonAncestor(root.left, p,q);
+        } else if (root.val < p.val && root.val < q.val) {
+            return lowestCommonAncestor(root.right, p,q);
         }
     }
 }
