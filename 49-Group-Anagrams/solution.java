9f5c2154047d25ae32236a66e7398154ade78417
Haolin Ju <hj836@nyu.edu>
2016-08-03 15:37:36 -0400

    [49. Group Anagrams][Accepted]committed by LeetCode Extension

--- 49-Group-Anagrams/solution.java
+++ 49-Group-Anagrams/solution.java
@@ -1,30 +1,34 @@
-// 3:00 - 3:24
 public class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
         List<List<String>> result = new ArrayList<>();
-        if (strs == null || strs.length == 0) 
+        
+        if (strs == null || strs.length == 0) {
             return result;
-        
-        HashMap<String, ArrayList<String>> map = new HashMap<>();
-        
-        for (int i = 0; i < strs.length; i++) {
-            char[] arr = strs[i].toCharArray();
-            Arrays.sort(arr);
-            String t = String.valueOf(arr);
-            if (!map.containsKey(t)) {
-                ArrayList<String> list = new ArrayList<>();
-                list.add(strs[i]);
-                map.put(t, list);
-            } else {
-                map.get(t).add(strs[i]);
-            }
         }
         
-        for (ArrayList<String> list: map.values()) {
+        HashMap<String, List<String>> map = new HashMap<>();
+        
+        for(String str : strs) {
+            String sortedStr = getSortedStr(str);
+            if(!map.containsKey(sortedStr)) {
+                List<String> list = new ArrayList<>();
+                
+                map.put(sortedStr, list);
+            }
+            
+            map.get(sortedStr).add(str);
+        }
+        
+        for(List<String> list : map.values()) {
             Collections.sort(list);
             result.add(list);
         }
         return result;
-        
+    }
+    
+    public String getSortedStr(String s) {
+        char[] chars = s.toCharArray();
+        Arrays.sort(chars);
+        return new String(chars);
     }
 }
