2ddec7abd64a64c28e864ed0f9ece2f596e4c9c0
Haolin Ju <hj836@nyu.edu>
2016-08-03 16:17:12 -0400

    [234. Palindrome Linked List][Accepted]committed by LeetCode Extension

--- 234-Palindrome-Linked-List/solution.java
+++ 234-Palindrome-Linked-List/solution.java
@@ -8,7 +8,7 @@
  */
 public class Solution {
     public boolean isPalindrome(ListNode head) {
-        if(head == null ||  head.next != null) {
+        if(head == null ||  head.next == null) {
             return true;
         }
         
