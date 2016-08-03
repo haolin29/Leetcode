20f5410a1be9b643ce9ba6f7d650cf6470c4af72
Haolin Ju <hj836@nyu.edu>
2016-08-03 15:20:43 -0400

    [215. Kth Largest Element in an Array][Accepted]committed by LeetCode Extension

--- 215-Kth-Largest-Element-in-an-Array/solution.java
+++ 215-Kth-Largest-Element-in-an-Array/solution.java
@@ -1,19 +1,46 @@
 public class Solution {
     public int findKthLargest(int[] nums, int k) {
         if(nums == null || nums.length == 0) {
-            return -1;
+            return 0;
         }
         
-        PriorityQueue<Integer> heap = new PriorityQueue<>(k);
+        return getKth(nums, 0, nums.length - 1, nums.length - k + 1);
+    }
+    
+    public int getKth(int[] nums, int start, int end, int k) {
+        int pivot = nums[end];
+        int left = start;
+        int right = end;
         
-        for(int num : nums) {
-            heap.offer(num);
-            
-            if(heap.size() > k) {
-                heap.poll();
+        while(true) {
+            while(left < right && nums[left] < pivot) {
+                left++;
             }
+            
+            while(left < right && nums[right] >= pivot) {
+                right--;
+            }
+            
+            if(left == right) break;
+            
+            swap(nums, left, right);
         }
         
-        return heap.peek();
+        // reset pivot 
+        swap(nums, left, end);
+        
+        if(left + 1 == k) {
+            return pivot;
+        } else if (left + 1 > k) {
+            return getKth(nums, start, left - 1, k);
+        } else {
+            return getKth(nums, left + 1, end ,k);
+        }
+    }
+    
+    public void swap(int[] nums, int a, int b) {
+        int temp = nums[a];
+        nums[a] = nums[b];
+        nums[b] = temp;
     }
 }
