342f9979ac99ea8d418354f6d472e0cb8c55bb37
Haolin Ju <hj836@nyu.edu>
2016-08-03 20:49:06 -0400

    [204. Count Primes][Time Limit Exceeded]committed by LeetCode Extension

--- 204-Count-Primes/solution.java
+++ 204-Count-Primes/solution.java
@@ -1,25 +1,23 @@
 public class Solution {
     public int countPrimes(int n) {
-        // Sieve of Eratosthenes 
-        boolean[] isPrime = new boolean[n];
-        for (int i = 2; i < n; i++) {
-            isPrime[i] = true;
-        }
-        
-        for (int i = 2; i * i < n; i++ ) {
-            if (!isPrime[i]) continue; // such as we can skip 4
-            for (int j = i*i; j < n; j+=i) {
-                isPrime[j] = false;
-            }
-        }
+        if (n < 1) return 0; 
         int count = 0;
-        for (int i = 2; i < n;i++) {
-            if (isPrime[i]) {
+        for(int i = 2; i < n; i++) {
+            if(isPrime(i)) {
                 count++;
             }
         }
         
         return count;
+    }
+    
+    public boolean isPrime(int num) {
+        for(int i = 2; i * i <= num; i++) {
+            if(num % i == 0) {
+                return false;
+            }
+        }
         
+        return true;
     }
 }
