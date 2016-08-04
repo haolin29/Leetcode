4de48dcedeb86ce5c42bcd951809144297dc6084
Haolin Ju <hj836@nyu.edu>
2016-08-03 21:11:51 -0400

    [199. Binary Tree Right Side View][Accepted]committed by LeetCode Extension

--- 199-Binary-Tree-Right-Side-View/solution.java
+++ 199-Binary-Tree-Right-Side-View/solution.java
@@ -10,32 +10,32 @@
 public class Solution {
     public List<Integer> rightSideView(TreeNode root) {
         List<Integer> result = new ArrayList<>();
-        if (root == null) {
+        
+        if(root == null) {
             return result;
         }
-
-        Queue<TreeNode> queue = new LinkedList<>();
-        queue.offer(root);
-
-        while(!queue.isEmpty()) {
-            int size = queue.size();
-
-            for (int i = 0; i < size; i++) {
-                TreeNode cur = queue.poll();
-
-                if (i == (size - 1)) {
-                    result.add(cur.val);
+        
+        Queue<TreeNode> q = new LinkedList<>();
+        q.offer(root);
+        
+        while(!q.isEmpty()) {
+            int size = q.size();
+            for(int i = 0; i < size; i++) {
+                TreeNode node = q.poll();
+                if(i == size - 1) {
+                    result.add(node.val);
                 }
-                if (cur.left != null) {
-                    queue.offer(cur.left);
+                
+                if(node.left != null) {
+                    q.offer(node.left);
                 }
-                if (cur.right != null) {
-                    queue.offer(cur.right);
+                
+                if(node.right != null) {
+                    q.offer(node.right);
                 }
-
             }
         }
-
+        
         return result;
     }
 }
