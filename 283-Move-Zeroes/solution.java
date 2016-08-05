c9e689144a09b3ee86c1fbd600459b4facaa2aaa
Haolin Ju <hj836@nyu.edu>
2016-08-05 15:04:08 -0400

    [283. Move Zeroes][Runtime Error]committed by LeetCode Extension

--- 283-Move-Zeroes/solution.java
+++ 283-Move-Zeroes/solution.java
@@ -5,24 +5,22 @@ public class Solution {
         }
         
         int p1 = 0;
-        int p2 = 0;
         int n = nums.length;
-        while(p1 < n && p2 < n) {
+        while(p1 < n) {
             while(p1 < n && nums[p1] != 0) {
                 p1++;
             }
             
+            int p2 = p1 + 1;
             while(p2 < n && nums[p2] == 0) {
                 p2++;
             }
             
-            if(p1 == n || p2 == n ) break;
+            if(p1 >= n - 1) break;
             
-            if (p1 < p2) {
-                int temp = nums[p1];
-                nums[p1] = nums[p2];
-                nums[p2] = temp;
-            }
+            int temp = nums[p1];
+            nums[p1] = nums[p2];
+            nums[p2] = temp;
         }
         
         
