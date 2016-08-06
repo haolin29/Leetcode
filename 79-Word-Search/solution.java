f7f853a479bc97d99f8d622b5a54d9736331b3ab
Haolin Ju <hj836@nyu.edu>
2016-08-05 20:48:28 -0400

    [79. Word Search][Accepted]committed by LeetCode Extension

--- 79-Word-Search/solution.java
+++ 79-Word-Search/solution.java
@@ -14,7 +14,7 @@ public class Solution {
         // iterate starting point
         for(int i = 0; i < n; i++) {
             for(int j = 0; j < board[0].length; j++) {
-                if(dfs(board, i, j, word, "",used)) {
+                if(dfs(board, i, j, word, 0,used)) {
                     return true;
                 }
             }
@@ -23,26 +23,21 @@ public class Solution {
         return false;
     }
     
-    public boolean dfs(char[][] board, int i, int j, String word, String sb, boolean[][] used) {
-        if(sb.length() == word.length()) {
-            if(sb.equals(word)) {
-                return true;
-            } else {
-                return false;
-            }
+    public boolean dfs(char[][] board, int i, int j, String word, int index, boolean[][] used) {
+        if(index == word.length()) {
+            return true;
         }
         
         
-        if(i < 0 || i >= board.length || j < 0 || j >= board[0].length || used[i][j]) {
+        if(i < 0 || i >= board.length || j < 0 || j >= board[0].length || used[i][j] || board[i][j] != word.charAt(index)) {
             return false;
         }
         
-        sb += board[i][j];
         used[i][j] = true;
-        boolean res = dfs(board, i - 1, j, word, sb, used) ||
-            dfs(board, i, j - 1, word, sb,used) ||
-            dfs(board, i + 1, j, word, sb,used) ||
-            dfs(board, i, j + 1, word, sb,used);
+        boolean res = dfs(board, i - 1, j, word, index + 1, used) ||
+            dfs(board, i, j - 1, word, index + 1,used) ||
+            dfs(board, i + 1, j, word, index + 1,used) ||
+            dfs(board, i, j + 1, word, index + 1,used);
         used[i][j] = false;
         
         return res;
