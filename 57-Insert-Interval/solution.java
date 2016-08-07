07ddb877ba416144f21438bb63844642726583dc
Haolin Ju <hj836@nyu.edu>
2016-08-07 15:31:59 -0400

    [57. Insert Interval][Runtime Error]committed by LeetCode Extension

---
+++ 57-Insert-Interval/solution.java
@@ -0,0 +1,40 @@
+/**
+ * Definition for an interval.
+ * public class Interval {
+ *     int start;
+ *     int end;
+ *     Interval() { start = 0; end = 0; }
+ *     Interval(int s, int e) { start = s; end = e; }
+ * }
+ */
+public class Solution {
+    public List<Interval> insert(List<Interval> intervals, Interval newInterval) {
+        List<Interval> result = new ArrayList<>();
+        if(intervals.size() == 0) {
+            result.add(newInterval);
+            return result;
+        }
+        
+        int index = 0;
+        while(index < intervals.size() && intervals.get(index).end < newInterval.start) {
+            result.add(intervals.get(index));
+            index++;
+        }
+        
+        newInterval.start = Math.min(intervals.get(index).start, newInterval.start);
+        
+        while(index < intervals.size() && intervals.get(index).start <= newInterval.end) {
+            newInterval.end = Math.max(intervals.get(index).end, newInterval.end);
+            index++;
+        }
+        
+        result.add(newInterval);
+        
+        while(index < intervals.size()) {
+            result.add(intervals.get(index));
+            index++;
+        }
+        
+        return result;
+    }
+}
