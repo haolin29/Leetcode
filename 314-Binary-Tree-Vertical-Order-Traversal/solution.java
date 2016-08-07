ad45e692fc6e54a58c7c1c4f2be89458cafecb3d
Haolin Ju <hj836@nyu.edu>
2016-08-06 21:22:07 -0400

    [314. Binary Tree Vertical Order Traversal][Accepted]committed by LeetCode Extension

--- 314-Binary-Tree-Vertical-Order-Traversal/solution.java
+++ 314-Binary-Tree-Vertical-Order-Traversal/solution.java
@@ -9,59 +9,39 @@
  */
 public class Solution {
     public List<List<Integer>> verticalOrder(TreeNode root) {
-        List<List<Integer>> result = new ArrayList<>();
-        if(root == null) {
-            return result;
-        }
-        
-        HashMap<Integer, List<Integer>> map = new HashMap<>();
-        List<Integer> list = new ArrayList<Integer>();
-        list.add(root.val);
-        map.put(0, list);
-        traversal(root, map, 0);
-        
-        int max = Integer.MIN_VALUE;
-        int min = Integer.MAX_VALUE;
-        for(Integer i : map.keySet()) {
-            max = Math.max(max, i);
-            min = Math.min(min, i);
-        }
-        
-        for(int i = max; i >= min; i-- ) {
-            result.add(map.get(i));
-        }
-        
-        return result;
-        
-    }
+        List<List<Integer>> res = new ArrayList<>();
+    	if(root == null) return res;
+    	
+    	Map<Integer, ArrayList<Integer>> map = new HashMap<>();
+    	Queue<TreeNode> q = new LinkedList<>();
+    	Queue<Integer> cols = new LinkedList<>();
     
-    public void traversal(TreeNode root, HashMap<Integer, List<Integer>> map, int col) {
-        if(root == null) {
-            return;
-        }
-        
-        if(root.left != null) {
-            if(!map.containsKey(col + 1)) {
-                List<Integer> list = new ArrayList<>();
-                list.add(root.left.val);
-                map.put(col + 1, list);
-            } else {
-                map.get(col + 1).insert(0,root.left.val);
-            } 
-        }
-        
-        if(root.right != null) {
-            if(!map.containsKey(col - 1)) {
-                List<Integer> list = new ArrayList<>();
-                list.add(root.right.val);
-                map.put(col - 1, list);
-            } else {
-                map.get(col - 1).insert(0,root.right.val);
-            }  
-        }
-        
-        traversal(root.left, map, col + 1);
-        traversal(root.right, map, col - 1);
-        
+    	q.add(root); 
+    	cols.add(0);
+    
+    	int min = 0, max = 0;
+    	while(!q.isEmpty()) {
+    		TreeNode node = q.poll();
+    		int col = cols.poll();
+    		if(!map.containsKey(col)) map.put(col, new ArrayList<Integer>());
+    		map.get(col).add(node.val);
+    
+    		if(node.left != null) {
+    			q.add(node.left); 
+    			cols.add(col - 1);
+    			if(col <= min) min = col - 1;
+    		}
+    		if(node.right != null) {
+    			q.add(node.right);
+    			cols.add(col + 1);
+    			if(col >= max) max = col + 1;
+    		}
+    	}
+    
+    	for(int i = min; i <= max; i++) {
+    		res.add(map.get(i));
+    	}
+    
+    	return res;
     }
 }
