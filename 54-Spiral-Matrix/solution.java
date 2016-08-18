609dc3609cc82a84aa5cffd9c43316efd38fd84a
Haolin Ju <hj836@nyu.edu>
2016-08-18 16:22:45 -0400

    [54. Spiral Matrix][Accepted]committed by LeetCode Extension

--- 54-Spiral-Matrix/solution.java
+++ 54-Spiral-Matrix/solution.java
@@ -1,36 +1,41 @@
 public class Solution {
     public List<Integer> spiralOrder(int[][] matrix) {
         List<Integer> result = new ArrayList<>();
-        if (matrix == null || matrix.length == 0) 
+        
+        if(matrix == null || matrix.length == 0) {
             return result;
-        int rows = matrix.length;
-        int cols = matrix[0].length;
-        
-        int count = 0;
-        
-        while (count * 2 < rows && count * 2 < cols) {
-            for (int i = count; i < cols - count; i++) {
-                result.add(matrix[count][i]);
-            }
-            
-            for (int i = count + 1; i < rows - count; i++) {
-                result.add(matrix[i][cols - count - 1]);
-            }
-            
-            if (rows - 2 * count == 1 || cols - 2 * count == 1) {
-                break;
-            }
-            
-            for (int i = cols - count - 2; i >= count; i--) {
-                result.add(matrix[rows - count - 1][i]);
-            }
-            
-            for (int i = rows - count - 2; i >= count + 1; i--) {
-                result.add(matrix[i][count]);
-            }
-            count++;
-            
         }
+        
+        int circle = 0;
+        int row = matrix.length;
+        int col = matrix[0].length; 
+        while(circle * 2 < row && circle * 2 < col) {
+            // top
+            for(int i = circle; i < col - circle; i++) {
+                result.add(matrix[circle][i]);
+            }
+            
+            // right
+            for(int i = circle + 1; i < row - circle; i++) {
+                result.add(matrix[i][col - circle - 1]);
+            }
+            
+            if(row - 2 * circle == 1 || col - 2 * circle == 1)  // if only one row /col remains
+                break;
+            
+            // bottom
+            for(int i = col - circle - 2; i >= circle; i--) {
+                result.add(matrix[row - circle - 1][i]);
+            }
+            
+            // left
+            for(int i = row - circle - 2; i >= circle + 1; i--) {
+                result.add(matrix[i][circle]);
+            }
+            
+            circle++;
+        }
+        
         return result;
     }
 }
