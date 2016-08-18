5feeb476aa617efeee849fba643d252d3a23583a
Haolin Ju <hj836@nyu.edu>
2016-08-17 20:15:51 -0400

    [189. Rotate Array][Accepted]committed by LeetCode Extension

--- 189-Rotate-Array/solution.java
+++ 189-Rotate-Array/solution.java
@@ -1,20 +1,22 @@
 public class Solution {
     public void rotate(int[] nums, int k) {
-        // three way reverse
-        if (nums == null || nums.length == 0) {
+        if(nums == null || nums.length == 0) {
             return;
         }
-
-        int len = nums.length;
-        k = k % len;
-        reverse(nums, 0, len - k - 1);
-        reverse(nums, len - k, len - 1);
-        reverse(nums, 0, len - 1);
         
+        
+        int n = nums.length;
+        
+        k = k % n;
+        
+        reverse(nums, 0, n - k - 1);
+        reverse(nums, n - k, n - 1);
+        reverse(nums, 0, n - 1);
     }
     
+    
     public void reverse(int[] nums, int start, int end) {
-        for (; start < end; start++, end--) {
+        for(; start < end; start++, end--) {
             int temp = nums[start];
             nums[start] = nums[end];
             nums[end] = temp;
