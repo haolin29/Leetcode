323d85af8257e23cf413443d8df0f93e139e90e2
Haolin Ju <hj836@nyu.edu>
2016-08-23 12:59:03 -0400

    [30. Substring with Concatenation of All Words][Accepted]committed by LeetCode Extension

--- 30-Substring-with-Concatenation-of-All-Words/solution.java
+++ 30-Substring-with-Concatenation-of-All-Words/solution.java
@@ -1,40 +1,75 @@
 public class Solution {
     public List<Integer> findSubstring(String s, String[] words) {
-        ArrayList<Integer> result = new ArrayList<Integer>();
-        HashMap<String,Integer> map = new HashMap<>();
-        HashMap<String,Integer> scan = new HashMap<>();
+        List<Integer> result = new ArrayList<>();
+        if(s == null || s.length() == 0 || words == null || words.length == 0) {
+            return result;
+        }
         
-        for (String word: words) {
-            if (!map.containsKey(word)) {
-                map.put(word,1);
+        Map<String, Integer> map = new HashMap<>();
+        for(String word : words) {
+            if(!map.containsKey(word)) {
+                map.put(word, 1);
             } else {
                 map.put(word, map.get(word) + 1);
             }
         }
         
-        int m = words.length, n = words[0].length();
-        
-        for (int i = 0; i <= s.length() - m * n;i++) {
-            scan.clear();
-            int j = 0;
-            for (; j < m; j++) {
-                int k = i + j * n;
-                String ss = s.substring(k, k + n);
-                if (!map.containsKey(ss) ) break;
-                if (!scan.containsKey(ss)) {
-                    scan.put(ss,1);
-                } else {
-                    scan.put(ss, scan.get(ss) + 1);
-                }
-                if (scan.get(ss) > map.get(ss) ) break;
-            }
+        for(int i = 0; i < words[0].length(); i++) {
+            int step = words[0].length();
+            int count = 0;
+            int left = i;
             
-            if (j == m) result.add(i);
+            HashMap<String, Integer> track = new HashMap<>();
+            
+            for(int j = i; j <= s.length() - step; j += step) {
+                String word = s.substring(j, j + step);
+                if(map.containsKey(word)) {
+                    if(!track.containsKey(word)) {
+                        track.put(word, 1);   
+                    } else {
+                        track.put(word, track.get(word) + 1);
+                    }
+                    
+                    if(track.get(word) <= map.get(word)) {
+                        count++;
+                    } else {
+                        // move left pointer
+                        while(track.get(word) > map.get(word)) {
+                            
+                            String str = s.substring(left, left + step);
+                            
+                            if(track.containsKey(str)) {
+                                track.put(str, track.get(str) - 1);
+                                if(track.get(str) < map.get(str)) {
+                                    count--;
+                                }
+                            }
+                            
+                            left += step;
+                        }
+                    }
+                    
+                    if(count == words.length) {
+                        result.add(left);
+                        
+                        String str = s.substring(left, left + step);
+                        if(track.containsKey(str)) {
+                            track.put(str, track.get(str) - 1);
+                            left += step;
+                            count--;
+                        }
+                    }
+                    
+                } else {
+                    track.clear();
+                    left = j + step;
+                    count = 0;
+                }
+                
+            }
         }
         
+        
         return result;
-        
-        
-        
     }
 }
